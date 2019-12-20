474c1238173df6e8281f1039dbbba4376aee6d60
monika-kh <khasdevmonika@gmail.com>
2019-12-20 18:47:30 +0530

    Services update.

--- student_app/services.py
+++ student_app/services.py
@@ -30,7 +30,6 @@ class GetCollegeService(Service):
 
 class DeleteCollegeService(Service):
     def process(self):
-
         pk = self.data.get('pk')
         college_dlt = College.objects.get(pk=pk)
         college_dlt.delete()
@@ -38,7 +37,6 @@ class DeleteCollegeService(Service):
 
 class PutCollegeService(Service):
     def process(self):
-
         college_put = self.data  # data received from views
         clg = college_put.get('data')
         pk = clg.get('id')
@@ -56,7 +54,7 @@ class PutCollegeService(Service):
 
 
 class GetStudentService(Service):
-    def process(self):
+    def process(self):  # get data of particular id of student
         std = self.data
         pk = std.get("pk")
         if pk:
@@ -74,30 +72,125 @@ class CreateStudentService(Service):
         data = self.data
         student_data = data.get("student_data")
         # get college object or college_id from college model or clg_service using student_data
-        clg_obj = College.objects.get(id=data.get("student_data").get("college"))
+        clg_obj = College.objects.get(college_name=data.get("student_data").get("college_name"))
+
+        # student_obj = Student.objects.create(
+        #     college=clg_obj,
+        #     first_name=student_data.get("first_name"),
+        #     last_name=student_data.get("last_name"),
+        #     branch=student_data.get("branch"),
+        #     dob=student_data.get("dob"),
+        # )
         student_obj = Student.objects.create(
             college=clg_obj,
-            first_name=student_data.get("first_name"),
-            last_name=student_data.get("last_name"),
-            branch=student_data.get("branch"),
-            dob=student_data.get("dob"),
+            first_name=student_data.get('college_student')[0].get('first_name'),
+            last_name=student_data.get('college_student')[0].get("last_name"),
+            branch=student_data.get('college_student')[0].get("branch"),
+            dob=student_data.get('college_student')[0].get("dob"),
+            username=student_data.get('college_student')[0].get("username"),
+            email=student_data.get('college_student')[0].get("email"),
+            address=student_data.get('college_student')[0].get("address"),
+            #college=student_data.get('college_student')[0].get("college")
         )
 
-        return student_obj
+        password = student_obj.set_password('password')
+        password.save()
+
+
+
+
 
 
 class GetRelatedStudentService(Service):
     def process(self):
         data = self.data
         pk = data.get("pk")
-
         related_name = Student.objects.filter(college_id=pk)  # get students related to a particular college id
         return related_name
 
     # query--> related_clg = Student.objects.filter(college_id=5)
 
 
-class GetRegisterService(Service):
+class DeleteRelatedStudentService(Service):
     def process(self):
-        student_register = Student.objects.all()
-        return student_register
+        dlt_data = self.data
+        pk = dlt_data.get('pk')
+        std_dlt = Student.objects.get(pk=pk)
+        std_dlt.delete()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class GetRegisterService(Service):
+#     def process(self):
+#         student_register = Student.objects.all()
+#         return student_register
+#
+#
+# class CreateRegisterService(Service):
+#     def process(self):
+#         college_student_data = self.data
+#         clg_std = college_student_data.get("register_data")
+#         college_data = clg_std.get('college')
+#         breakpoint()
