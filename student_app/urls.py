49fec8195aa339c383a329bb88e33a63e5d563eb
monika-kh <khasdevmonika@gmail.com>
2019-12-21 17:10:11 +0530

    Update app-url.

--- student_app/urls.py
+++ student_app/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from student_app import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('college/', views.CollegeView.as_view()),
-    path('college/<int:pk>/', views.CollegeView.as_view(), name='college'),
-    path('student/', views.StudentView.as_view()),
-    path('student/<int:pk>/', views.StudentView.as_view(), name='student')
+    path("college/", views.CollegeView.as_view()),
+    path("college/<int:pk>/", views.CollegeView.as_view(), name="college"),
+    path("student/", views.StudentView.as_view()),
+    path("student/<int:pk>/", views.StudentView.as_view(), name="student"),
+    path('login_api/', obtain_auth_token, name='api_token_auth'),
+    #path('rest-auth/', include('rest_auth.urls')),
 ]
+
+
+
