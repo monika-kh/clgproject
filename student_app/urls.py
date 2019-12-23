4d10e773980b3a2a334c899872f990695ce57e29
monika-kh <khasdevmonika@gmail.com>
2019-12-23 13:45:38 +0530

    Urls.

--- student_app/urls.py
+++ student_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("student/", views.StudentView.as_view()),
     path("student/<int:pk>/", views.StudentView.as_view(), name="student"),
     path('login_api/', obtain_auth_token, name='api_token_auth'),
+    path("login/", views.LogInView.as_view()),
     #path('rest-auth/', include('rest_auth.urls')),
 ]
 
