6f76b0d31d0e41bbee4e113dfbb1cf1e56c23b73
monika-kh <khasdevmonika@gmail.com>
2019-12-28 19:04:28 +0530

    app_url.py updated.

--- student_app/urls.py
+++ student_app/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path("student/<int:pk>/", views.StudentView.as_view(), name="student"),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path("send-mail/", views.SendEmailView.as_view()),
-    #path("index", views.IndexView.as_view(), name='index'),
+    path("index", views.IndexView.as_view(), name='index'),
+    path("college_by_name/", views.PrintCollegeNameView.as_view()),
     #path('rest-auth/', include('rest_auth.urls')),
 ]
 
